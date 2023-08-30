bc3e8132af93a2779db765e53bb6277a588e0c2d
Wang HongMing <superwangwang0603@gmail.com>
2023-08-30 20:44:59 +0800

    change auth

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,5 @@ Route::get('/product/create', [ProductController :: class, 'create'])->name('pro
 Route::post('/product', [ProductController :: class, 'store'])->name('product.store');
 Route::get('/product/{product}/edit', [ProductController :: class, 'edit'])->name('product.edit');
 Route::put('/product/{product}/update', [ProductController :: class, 'update'])->name('product.update');
-Route::delete('/product/{product}/destroy', [ProductController :: class, 'destroy'])->name('product.destroy');
+Route::delete('/product/{product}/destroy', [ProductController :: class, ''])->name('product.destroy');
 
